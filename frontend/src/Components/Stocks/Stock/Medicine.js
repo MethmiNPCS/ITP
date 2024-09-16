a9929830881e2a5ba10a37155b1d00cf933607c8
MethmiNPCS <npcsmethmi@gmail.com>
2024-09-16 10:11:46 +0530

    Stocks
    Uploaded both frontend, backend related to stocks. This is the folder structure

---
+++ frontend/src/Components/Stocks/Stock/Medicine.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../../Stocks/Stock.css';
+import { Link } from 'react-router-dom';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+
+function Medicine(props) {
+  const { _id, name, animal, type, EXD, quantity, unit, instructions } = props.medicine;
+
+  const history = useNavigate();
+
+  const deleteHandler = async () => {
+    await axios.delete(`http://localhost:5000/stocks/${_id}`)
+      .then(() => {
+        window.location.reload(); // Force a full page reload
+      });
+  };
+
+  return (
+    <div className="stock-container"> 
+      <h1 className='small-heading'>Medicine Details</h1>
+      <br />
+      <p><b>ID : </b>{_id}</p>
+      <p><b>Name: </b>{name}</p>
+      <p><b>Animal : </b>{animal}</p>
+      <p><b>Stock type : </b>{type}</p>
+      <p><b>Entry Date : </b>{EXD}</p>
+      <p><b>Quantity : </b>{quantity} {unit}</p>
+      <p><b>Instructions : </b>{instructions}</p>
+      <Link to={`/medicinedetails/${_id}`}>
+        <button className="update-button">Update</button>
+      </Link>
+      <button className="delete-button" onClick={deleteHandler}>Delete</button>
+    </div>
+  );
+}
+
+export default Medicine;
---
+++ frontend/src/Components/Stocks/StockHome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav/Nav';
+
+function StockHome() {
+  return (
+    <div>
+        <Nav/>
+      <h1>This is Stock Home</h1>
+    </div>
+  );
+}
+
+export default StockHome;

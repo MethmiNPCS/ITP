e6814ec2d3f15d69d60b88286105038edde016f8
MethmiNPCS <npcsmethmi@gmail.com>
2024-09-16 10:36:14 +0530

    Folder structure

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+// MongoDB password = zoPvf0NUih9wBU3F
+// Database name = cluster0
+// MongoDB project name = farm-systemDB
+
+const express = require("express");
+const mongoose = require("mongoose");
+const stockrouter = require("./Routes/StockRoutes");
+
+const app = express();
+const cors = require("cors");
+
+// Middlewear
+app.use(express.json());
+app.use(cors());
+app.use("/stocks", stockrouter);
+
+// Connect Database
+mongoose.connect("mongodb+srv://admin:zoPvf0NUih9wBU3F@cluster0.yawwn.mongodb.net/")
+.then(()=> console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err)=> console.log(err));

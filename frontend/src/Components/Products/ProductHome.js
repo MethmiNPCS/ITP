6b0f3b5e1292de7481efb4681379b2732b3e40ca
udumu <it22277640@my.sliit.lk>
2024-09-23 16:00:00 +0530

    Visu bac and front

---
+++ frontend/src/Components/Products/ProductHome.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Nav from './Nav/Nav';
+import img from '../img/farm.jpg';
+
+function ProductHome() {
+  const backgroundImageStyle = {
+    backgroundImage: 'url(/images/farm.jpg)', // Path inside 'public' folder
+    backgroundSize: 'cover', // Ensures the image covers the entire div
+    backgroundPosition: 'center', // Center the background image
+    backgroundRepeat: 'no-repeat', // Prevent repeating the background image
+    minHeight: '50vh', // Full height of the viewport
+    width: '100%', // Full width of the viewport
+  };
+
+  return (
+    <div style={backgroundImageStyle}>
+      <center>
+        <Nav />
+       
+        <div>
+          {/* Display product image */}
+          <img src={img} alt="Product" style={{ maxWidth: '100%', height: 'auto' }} />
+        </div>
+      </center>
+    </div>
+  );
+}
+
+export default ProductHome;

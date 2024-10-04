dcc171a8af313c1f477d6f10d96642f5bdfebd95
MethmiNPCS <npcsmethmi@gmail.com>
2024-10-04 12:17:52 +0530

    Small changes

--- frontend/src/Components/Stocks/StockDetails/Foods.js
+++ frontend/src/Components/Stocks/StockDetails/Foods.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 import '../../Stocks/Stock.css';
 import { Link } from 'react-router-dom';
 import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
 
 const URL = "http://localhost:5000/stocks";
 

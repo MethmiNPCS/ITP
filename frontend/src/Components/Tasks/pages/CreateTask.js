7d3a91ba25a83fe56b08007c2de830db57439d4c
D.L <140206024+dhiyanahL@users.noreply.github.com>
2024-10-05 15:15:07 +0530

    made a bg colour change

--- frontend/src/Components/Tasks/pages/CreateTask.js
+++ frontend/src/Components/Tasks/pages/CreateTask.js
@@ -66,7 +66,7 @@ const CreateTask = () => {
     };
 
     return (
-        <div className="p-4">
+        <div className="p-4 bg-green-200">
             <h1 className="text-3xl my-4 text-center font-serif">Create Task</h1>
             <div className="bg-white flex flex-col border-2 border-dark-green rounded-xl w-[600px] p-4 mx-auto bg-light-green">
                 <div className="my-4">

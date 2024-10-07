cafbe33426be5ce833fb7e6780f117410ccc6ff1
sayu-codespace <sayuruwanyam@gmail.com>
2024-10-07 13:54:33 +0530

    frontend nav bar added a new id

--- frontend/src/Components/Orders/Nav/Nav.js
+++ frontend/src/Components/Orders/Nav/Nav.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <div className='nvbody'>
+    <div className='nbbody'>
     <nav className="navbar">
       <ul className="nav-list">
         
